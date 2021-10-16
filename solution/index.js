3dae65d679f64545c2f18567b4ba6e0795586932
Andrey Adamovich <gooddha@ya.ru>
2021-10-16 18:31:14 +0300

    done reduce function

--- solution/index.js
+++ solution/index.js
@@ -1,37 +1,42 @@
 module.exports = function (Homework) {
 
   const { AsyncArray, add, subtract, multiply, divide, less, equal, lessOrEqual } = Homework;
-  let getLength = async (array) => {
+  const getLength = async (array) => {
     return await new Promise((resolve) => {
       array['length']((res) => resolve(res));
     });
-  }
+  };
 
-  let getValue = async (i, array) => {
+  const getValue = async (i, array) => {
     return await new Promise((resolve) => {
       array.get(i, (res) => resolve(res));
     });
-  }
+  };
 
-  let calc = async (method, a, b) => {
+  const calc = async (method, a, b) => {
     return await new Promise((resolve) => {
       method(a, b, (res) => resolve(res));
     });
+  };
+
+  const doFn = async (fn, acc, current, i, src) => {
+    return await new Promise(resolve => {
+      fn(acc, current, i, src, (res) => resolve(res));
+    })
   }
+
 
   return async (asyncArray, fn, initialValue, cb) => {
 
-    let length = await getLength(asyncArray)
+    const length = await getLength(asyncArray)
 
     let acc = initialValue;
     let i = 0;
 
     while (await calc(less, i, length)) {
       let current = await getValue(i, asyncArray);
-      console.log(current)
 
-      acc = await calc(add, current, acc)
-
+      acc = await doFn(fn, acc, current, i, asyncArray);
       i = await calc(add, 1, i);
     }
 
